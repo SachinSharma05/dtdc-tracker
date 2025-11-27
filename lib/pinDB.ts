3517edd2ce5961343f22428a39bd8dd4b27981c5
SachinSharma05 <sachin.sharma0583@gmail.com>
2025-11-27 22:48:41 +0530

    bug fix-20

--- lib/pinDB.ts
+++ lib/pinDB.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import * as indian_pincodes from "../db/";
+import * as indian_pincodes from "../db/pincodeSchema";
 
 // Load local SQLite file
 const sqlite = new Database("data/pincode/pincodes.sqlite");
